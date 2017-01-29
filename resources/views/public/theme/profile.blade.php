922c01b3757165e5ea215295e5e719cb556ea03f
vagrant <vagrant@homestead>
2017-01-29 10:35:19 +0000

    Update default theme

--- resources/views/public/theme/profile.blade.php
+++ resources/views/public/theme/profile.blade.php
@@ -21,41 +21,47 @@
               <div class="user-bio">
                 <p><strong>USERNAME:</strong> {{$user->username}}</p>
                 <p><strong>EMAIL:</strong> {{$user->email}}</p>
-                <p><strong>ROLE:</strong> {{$user->role->name}}</p>
+                <p><strong>ROLE:</strong> {{isset($user->role) ? $user->role->name : ''}}</p>
                 <p><strong>JOINED:</strong> {{$user->created_at->diffForHumans()}}</p>
                 <p><strong>BIO:</strong> {{$user->bio}}</p>
               </div>
-              @if (!empty($user->articles))
-                <a href="{{route('article', $user->articles->first()->slug)}}">
-                  <div class="user-latest">
-                    <div class="title">
-                      <h3>{{$user->articles->first()->name}}</h3>
+              <div class="related content">
+                @if (!empty($user->articles))
+                  <br />
+                  <h2>NEW</h2>
+                  <a href="{{route('article', $user->articles->first()->slug)}}">
+                    <div class="user-latest">
+                      <div class="title">
+                        <h3>{{$user->articles->first()->name}}</h3>
+                      </div>
+                      <div class="excerpt">
+                        <p>{{$user->articles->first()->excerpt}}</p>
+                      </div>
+                      <div class="thumbnail" style="background: url('{{isset($user->articles->first()->thumbnailImage) ? $user->articles->first()->thumbnailImage->url : ""}}') no-repeat; background-size: cover; background-position-y: 50%;" alt="{{$user->articles->first()->name}}"></div>
                     </div>
-                    <div class="excerpt">
-                      <p>{{$user->articles->first()->excerpt}}</p>
+                  </a>
+                  <div class="user-all">
+                    <br />
+                    <h2>WRITTEN BY {{strtoupper($user->name)}}</h2>
+                    <div class="row">
+                      @foreach ($user->articles as $a)
+                        <a href="{{route('article', $a->slug)}}">
+                          <div class="single col-sm-4">
+                            <p>
+                              <strong>{{$a->published_at->format('d.m. Y')}}</strong>
+                            </p>
+                            <h3>
+                              {{$a->name}}
+                            </h3>
+                          </div>
+                        </a>
+                      @endforeach
                     </div>
-                    <div class="thumbnail" style="background: url('{{$user->articles->first()->thumbnailImage->url}}" alt="{{$user->articles->first()->name}}') no-repeat; background-size: cover;"></div>
                   </div>
-                </a>
-                <div class="user-all">
-                  <div class="row">
-                    @foreach ($user->articles as $a)
-                      <a href="{{route('article', $a->slug)}}">
-                        <div class="single col-sm-4">
-                          <p>
-                            <strong>{{$a->published_at->format('d.m. Y')}}</strong>
-                          </p>
-                          <h3>
-                            {{$a->name}}
-                          </h3>
-                        </div>
-                      </a>
-                    @endforeach
-                  </div>
-                </div>
-              @else
-                <p>No articles.</p>
-              @endif
+                @else
+                  <p>No articles.</p>
+                @endif
+              </div>
             </div>
           </div>
           <div id="sidebar" class="article-single-sidebar col-sm-3">
