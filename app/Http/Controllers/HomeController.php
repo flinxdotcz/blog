ff9a0d0edb9a8e4801a6d4b45f7f574dfe274b84
vagrant <vagrant@homestead>
2017-01-29 15:51:18 +0000

    Fix articles filter

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,8 +15,8 @@ class HomeController extends Controller
   public function index() {
     $tags = Tag::displayed()->orderBy('order')->get();
     $main = Article::published()->orderBy('published_at', 'desc')->firstOrFail();
-    $latest = Article::published()->take(10)->orderBy('published_at', 'desc')->skip(1)->get();
-    $trending = Article::published()->take(10)->where('hits', '>', 0)->orderBy('hits', 'desc')->get();
+    $latest = Article::published()->take(10)->orderBy('published_at', 'desc')->where('id', '!=', $main->id)->get();
+    $trending = Article::published()->take(10)->where('hits', '>', 0)->where('id', '!=', $main->id)->orderBy('hits', 'desc')->get();
     return view('home', compact('tags', 'main', 'latest', 'trending'));
   }
 }
